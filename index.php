e7b8c75f5f1746e7f1d8871126382b44397e3045
Daniel Oliveira <danielsapup3@gmail.com>
2021-07-20 11:23:07 -0300

    Projeto criado

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Sistema de Perguntas Aleatorias</title>
+    </head>
+    <body>
+        
+    </body>    
+</html>
